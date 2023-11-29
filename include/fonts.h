4c585d4ef719f746e0765e4717d1fb55a25c0e08
Y3rw1N <yerwincuesta27@gmail.com>
2023-11-28 22:38:18 -0500

    fixed lol

--- include/fonts.h
+++ include/fonts.h
@@ -5,5 +5,5 @@
 
 #define HOME_DIRECTORY "/data/data/com.termux/files/home"
 #define FONTS_DIRECTORY HOME_DIRECTORY "/.termux/fonts"
- #define cascadia_code "wget -O "FONTS_DIRECTORY"/cascadia-code.ttf https://github.com/ryanoasis/nerd-fonts/blob/master/patched-fonts/CascadiaCode/Regular/CaskaydiaCoveNerdFontMono-Regular.ttf"
+ #define cascadia_code "wget -O "FONTS_DIRECTORY"/cascadia-code.ttf https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/CascadiaCode/Regular/CaskaydiaCoveNerdFontMono-Regular.ttf"
 #define fira_code "wget -O "FONTS_DIRECTORY"/fira-code.ttf https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/FiraCode/Regular/FiraCodeNerdFontMono-Regular.ttf"
