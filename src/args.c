d0d2ce4cf0584297aa5bdea30d83695bb6315d7e
Y3rw1N <yerwincuesta27@gmail.com>
2023-11-30 18:45:44 -0500

    bug fixed lol

--- src/args.c
+++ src/args.c
@@ -5,12 +5,10 @@ void help_message() {
 }
 
 void set_font(char *fontName) {
-	if (access(fontName, F_OK) != -1) {
-    //  char fontSet[200];
-        char comando[200];
-    //  snprintf(fontSet, sizeof(fontSet), TERMUX_DIRECTORY "/font.ttf");
-        snprintf(comando, sizeof(comando), "cp " FONTS_DIRECTORY "/%s.ttf "FONT_SET_DEFAULT"", fontName);
-        system(comando);
+	if (access(FONTS_DIRECTORY, F_OK) != -1) {
+        char command[200];
+        snprintf(command, sizeof(command), "cp %s/%s.ttf %s", FONTS_DIRECTORY, fontName, FONT_SET_DEFAULT);
+        system(command);
         system("termux-reload-settings");
 	}
 }
