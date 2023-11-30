ece45a47d3ca2227cd7b4e9349d471ce22d79727
Y3rw1N <yerwincuesta27@gmail.com>
2023-11-30 18:32:04 -0500

    bug fixed lol

--- include/utils.h
+++ include/utils.h
@@ -8,6 +8,7 @@
 #define HOME_DIRECTORY "/data/data/com.termux/files/home"
 #define TERMUX_DIRECTORY HOME_DIRECTORY "/.termux"
 #define FONTS_DIRECTORY TERMUX_DIRECTORY "/fonts"
+#define FONT_SET_DEFAULT TERMUX_DIRECTORY "/font.ttf"
 
 void set_font();
 void help_message();
--- src/args.c
+++ src/args.c
@@ -1,15 +1,15 @@
 #include "../include/utils.h"
 
 void help_message() {
-	printf("\n  \'-S <fontname>\' to set the font.\n");
+	printf("\n  \'-S <fontname>\' to set the font.\n nm");
 }
 
 void set_font(char *fontName) {
 	if (access(fontName, F_OK) != -1) {
-        char fontSet[200];
+    //  char fontSet[200];
         char comando[200];
-        snprintf(fontSet, sizeof(fontSet), TERMUX_DIRECTORY "/font.ttf");
-        snprintf(comando, sizeof(comando), "cp " FONTS_DIRECTORY "/%s.ttf %s", fontName, fontSet);
+    //  snprintf(fontSet, sizeof(fontSet), TERMUX_DIRECTORY "/font.ttf");
+        snprintf(comando, sizeof(comando), "cp " FONTS_DIRECTORY "/%s.ttf "FONT_SET_DEFAULT"", fontName);
         system(comando);
         system("termux-reload-settings");
 	}
