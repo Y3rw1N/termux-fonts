0452abceeca8aa8ac9ed5e944ae2afafc1eae444
Y3rw1N <yerwincuesta27@gmail.com>
2023-11-30 22:12:22 -0500

    bug fixed lol

--- src/args.c
+++ src/args.c
@@ -4,11 +4,11 @@ void help_message() {
 	printf("\n  \'-S <fontname>\' to set the font.\n");
 }
 
-void set_font(char *fontName) {
-	if (access(fontName, F_OK) != -1) {
-        char command[200];
-        snprintf(command, sizeof(command), "cp /data/data/com.termux/files/home/.termux/%s.ttf %s",fontName,FONT_SET_DEFAULT);
-        system(command);
-        system("termux-reload-settings");
-	}
+void set_font(char* name) {
+  if (access(name, F_OK) != -1) {
+    char command[200];
+    snprintf(command, sizeof(command), "cp /data/data/com.termux/files/home/.termux/%s.ttf %s", name, FONT_SET_DEFAULT);
+    system(command);
+    system("termux-reload-settings");
+	}      
 }
