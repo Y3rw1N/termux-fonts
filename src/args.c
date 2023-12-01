8e79b4298e3579fff71d2fa3ca85511883abc2e6
Y3rw1N <yerwincuesta27@gmail.com>
2023-12-01 13:53:18 -0500

    bug fix

--- src/args.c
+++ src/args.c
@@ -7,7 +7,7 @@ void help_message() {
 void set_font(char* name) {
   if (access(name, F_OK) != -1) {
     char command[200];
-    snprintf(command, sizeof(command), "cp /data/data/com.termux/files/home/.termux/%s.ttf %s", name, FONT_SET_DEFAULT);
+    snprintf(command, sizeof(command), "cp /data/data/com.termux/files/home/.termux/%s.ttf %s", FONT_SET_DEFAULT, name);
     system(command);
     system("termux-reload-settings");
 	}      
