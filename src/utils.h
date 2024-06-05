87ea1dcb6597ac6d1b434d11aa714ec31e9c1d0a
Y3rw1N <yerwincuesta27@gmail.com>
2024-06-05 08:31:45 -0500

    Fixed "remove" argument

--- src/utils.h
+++ src/utils.h
@@ -12,9 +12,8 @@
 #define FONTS_DIRECTORY TERMUX_DIRECTORY "/fonts"
 #define FONT_SET_DEFAULT TERMUX_DIRECTORY "/font.ttf"
  
-bool set_font(const char *font_default);
-bool font_remove(const char *font_default);
 bool is_font_exists(const char *font_default);
-
+void set_font(const char *font_default);
+void font_remove(const char *font_default);
 
 #endif // UTILS_H //
